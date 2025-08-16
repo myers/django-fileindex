0cccc04ccb490f864287b2ab7b5c970969f97173
Myers Carpenter <myers@maski.org>
2025-08-16 15:18:15 -0400

    initial commit

--- fileindex/urls.py
+++ fileindex/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
 
-from .views import IndexedFileDetail, lookup, add, raw_file
+from .views import (
+    FilesWithoutMetadataView,
+    IndexedFileDetail,
+    VideoMetadataIssuesView,
+    add,
+    lookup,
+)
 
 app_name = "fileindex"
 urlpatterns = [
     path("lookup", lookup, name="lookup"),
     path("add", add, name="add"),
     path("files/<int:pk>/", IndexedFileDetail.as_view(), name="detail"),
-    path("files/<str:sha512>", raw_file, name="raw_file"),
+    path("admin/no-metadata/", FilesWithoutMetadataView.as_view(), name="no_metadata"),
+    path("admin/video-issues/", VideoMetadataIssuesView.as_view(), name="video_issues"),
 ]
